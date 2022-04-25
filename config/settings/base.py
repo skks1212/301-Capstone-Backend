a8cdf4a4ed278d7d99ed836949bc80d8199c4690
Shivank Kacker <kacker.shivank@gmail.com>
2022-04-25 23:33:54 +0530

    Changed Base variable

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,17 +43,14 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-#DATABASES = {"default": env.db("DATABASE_URL", default="postgres://capstone")}
-DATABASES = {
-    'default': {
+DATABASES = {"default": env.db("DATABASE_URL", default={
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'capstone',
         'USER': 'postgres',
         'PASSWORD': 'newpassword',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-    }
-}
+    })}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
