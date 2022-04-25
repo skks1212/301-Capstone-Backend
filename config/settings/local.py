2c2f7652172c6de9492cb2bcc2c7137312457173
Shivank Kacker <kacker.shivank@gmail.com>
2022-04-26 00:06:16 +0530

    Changed Base variable

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="aAarXqFtmCXWO5notz0NPrbrkeQurebBulGZibMyvxLxheuzTiksFE69of4aePYy",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
